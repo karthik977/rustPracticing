c1f1a6305bcff012627bc846676d903f822aa0b4
Karthikeya Gannoju <karthikeya@indicnode.ai>
2026-04-07 16:06:46 +0530

    added control flow

--- main.rs
+++ main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello World!");
+    let n = 5;
+
+    if n < 0 {
+        print!("{} is negative", n);
+    } else if n > 0 {
+        print!("{} is positive", n);
+    } else {
+        print!("{} is zero", n);
+    }
+
 }
